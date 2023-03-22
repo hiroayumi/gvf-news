afe09838d62d5a6a84d38d26a73591923fc433de
Angela Wang <119788524+hiroayumi@users.noreply.github.com>
2023-03-22 23:53:47 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-// static/scripts.js
+async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return await response.json();
+}
+
 document.getElementById("summarize").addEventListener("click", async () => {
     const inputText = document.getElementById("input-text").value;
     if (!inputText) return;
 
-    const response = await fetch("/api/summarize", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text: inputText }),
-    });
-
-    const { summary } = await response.json();
-    document.getElementById("output-summary").value = summary;
+    try {
+        const result = await postData('/api/summarize', { text: inputText });
+        document.getElementById("output-summary").value = result.summary;
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred while processing the summary. Please try again.');
+    }
 });
 
 document.getElementById("translate").addEventListener("click", async () => {
     const inputText = document.getElementById("output-summary").value;
     if (!inputText) return;
 
-    const response = await
-    fetch("/api/translate", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ text: inputText }),
-    });
-
-    const { translation } = await response.json();
-    document.getElementById("output-translation").value = translation;
+    try {
+        const result = await postData('/api/translate', { text: inputText });
+        document.getElementById("output-translation").value = result.translation;
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred while processing the translation. Please try again.');
+    }
 });
-
