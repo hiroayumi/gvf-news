f4aa09baabb3330bbac391c193f53471a28699ab
Angela Wang <119788524+hiroayumi@users.noreply.github.com>
2023-03-28 21:46:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,21 +11,24 @@ def gpt3_summarize(text):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-                 {"role": "system", "content": "You are a helpful summarizer"},
-                 {"role": "user", "content": """Please learn the writing style of the following template summary, and summarize a news article in a similar way.
-                                                The output should be at roughly the same length as the summary templates provided. 
-                                                The output should contain similar types of information as the summary templates do. 
-                                                The output should contain both the title as a separate line and the news body
-                                                Output: Cast AI raises $20m in funding 
-                                                Cast AI, a Miami, FL-based cloud-native automation and cost management startup, raised $20M in funding. The round was led by Creandum. 
-                                                The company intends to use the funds to expand operations and its business reach. Led by CEO Yuri Frayman, Cast AI is an all-in-one cloud-native automation platform that cuts cloud bills in half for AWS, GCP, and Azure customers. The platform employs AI and automation 
-                                                to analyze compute resources and optimize them in minutes. By connecting their Kubernetes clusters to the platform, organizations can 
-                                                see suggested recommendations and use cloud-native automation techniques for immediate cost reduction. Since the platform launch, 
-                                                CAST AI has experienced quarter-by-quarter revenue growth of over 220%, based on the company’s ability to provide optimization solutions 
-                                                that simplify cloud-native application management, a much-needed service in today’s tech-driven world. 
-                                                The template ends here. Please follow the template and summarize the following article"""},
-                 {"role": "user", "content": 'The article is "{text}"'} 
-                 ],
+            {"role": "system", "content": "You are a helpful summarizer"},
+            {"role": "user", "content": "Please learn the writing style of the following template summary, "
+             + "and summarize a news article in a similar way."
+             + "The output should be at roughly the same length as the summary templates provided. " 
+             + "The output should contain similar types of information as the summary templates do. "
+             + "The output should contain both the title as a separate line and the news body "
+             + "Output: Cast AI raises $20m in funding "
+             + "Cast AI, a Miami, FL-based cloud-native automation and cost management startup, raised $20M in funding. "
+             + "The round was led by Creandum. The company intends to use the funds to expand operations and its business reach. "
+             + "Led by CEO Yuri Frayman, Cast AI is an all-in-one cloud-native automation platform that cuts cloud bills in half "
+             + "for AWS, GCP, and Azure customers. The platform employs AI and automation to analyze compute resources and optimize "
+             + "them in minutes. By connecting their Kubernetes clusters to the platform, organizations can see suggested "
+             + "recommendations and use cloud-native automation techniques for immediate cost reduction. Since the platform launch, "
+             + "CAST AI has experienced quarter-by-quarter revenue growth of over 220%, based on the company’s ability to provide "
+             + "optimization solutions that simplify cloud-native application management, a much-needed service in today’s tech-driven world. "
+             + "The template ends here. Please follow the template and summarize the following article"},
+            {"role": "user", "content": 'The article is "{text}"'} 
+        ],
         temperature=0.5,
     )
     return completion['choices'][0]['message']['content']
@@ -35,9 +38,9 @@ def gpt3_translate(text):
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-                 {"role": "system", "content": "You are a professional translator"},
-                 {"role": "user", "content": 'Translate the following English text to Simplified Chinese: "{text}"'}
-                 ],
+            {"role": "system", "content": "You are a professional translator"},
+            {"role": "user", "content": 'Translate the following English text to Simplified Chinese: "{text}"'}
+        ],
         temperature=0.5,
     )
     return response['choices'][0]['message']['content']
